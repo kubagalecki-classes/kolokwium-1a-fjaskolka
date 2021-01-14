1fd1be2d6244fc29bb21faaa15c2294ac7ff7c51
fjaskolka <filipjask512@gmail.com>
2021-01-14 12:28:21 +0000

    z1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -22,10 +22,10 @@ class Tagliatelle : public Makaron
   static const double C;
 
   public:
-  Tagliatelle(double l, double w, double r) : L(l), W(w), R(r) {}
-  Tagliatelle() : L(0.5), W(0.5), R(0.5){}
+  Tagliatelle(double l, double w, double r): L(l), W(w), R(r) {}
+  Tagliatelle(): L(0.5), W(0.5), R(0.5){}
   double ileMaki(unsigned P) const 
   {
     return P * L * W * (1.-R) * C;
-  }
+  };
 };
